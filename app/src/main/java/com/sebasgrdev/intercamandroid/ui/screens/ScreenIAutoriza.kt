fa418fa9b196730f2888263c400b876585f8eaea
= <sebastianglez05@gmail.com>
2024-10-02 15:20:29 -0600

    first commit

---
+++ app/src/main/java/com/sebasgrdev/intercamandroid/ui/screens/ScreenIAutoriza.kt
@@ -0,0 +1,9 @@
+package com.sebasgrdev.intercamandroid.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScreenIAutoriza(modifier : Modifier = Modifier) {
+
+}
